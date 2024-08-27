53adaf7cf27b88ad4b71a93be23a96f47de302e5
subodh-1 <choure.subodh@gmail.com>
2024-08-27 11:41:57 +0530

    iter package created for iteration

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/subodh-1/gopractice
+
+go 1.22.0
+
+require github.com/wkschwartz/iter v0.0.0-20130915044707-3ce739894e9c // indirect
---
+++ iter/iter.go
@@ -0,0 +1,23 @@
+// iter.go
+package iter
+
+// Seq[T] is a sequence type that allows iteration over elements of type T.
+type Seq[T any] func(yield func(T) bool)
+
+// Channel converts a Seq[T] to a channel of T, allowing iteration using range.
+func (s Seq[T]) Channel() <-chan T {
+	ch := make(chan T)
+	go func() {
+		defer close(ch)
+		s(func(item T) bool {
+			ch <- item
+			return true
+		})
+	}()
+	return ch
+}
+
+// Allow iteration over the Seq[T] using range.
+func (s Seq[T]) Iterator() <-chan T {
+	return s.Channel()
+}
